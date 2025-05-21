02f59e2018f10882edffb4ae5ec4682400e5b6ce
marcio-guimaraes <mv601200@gmail.com>
2025-05-21 10:20:26 -0300

    Fila

---
+++ Semana 7 - Complexidade e Listas/G.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+
+
+    int quantidade;
+    scanf("%d", &quantidade);
+    int hambuguers[quantidade];
+
+    for (int i = 0; i < quantidade; i++)
+    {
+        scanf("%d", &hambuguers[i]);   
+    }
+    
+
+    int primos[10]{}
+
+
+
+    return 0;
+}
